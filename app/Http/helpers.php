a4856eb8ac81febc90c91dea0b987dd4e453737a
unknown <nlad@admin-PC675.citruspayho.com>
2015-12-31 02:35:06 +0530

    solved bug

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -4,7 +4,7 @@ function thumbnailImage($imagePath) {
     $imagick = new \Imagick(realpath($imagePath));
     $imagick->setbackgroundcolor('rgb(64, 64, 64)');
     $imagick->thumbnailImage(300, 300, true, true);
-    header("Content-Type: image/jpg");
+    //header("Content-Type: image/jpg");
     return $imagick->getImageBlob();
 }
 
